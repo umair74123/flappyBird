0a9bad0ccfa6074c523252e31f9c280124e57464
umair74123 <umairabbasi74123@gmail.com>
2024-10-30 19:14:18 +0500

    update

---
+++ lib/game/configuration.dart
@@ -0,0 +1,8 @@
+class Config {
+  static const groundHeight = 110.0;
+  static const gameSpeed = 200.0;
+  static const pipeInterval = 1.5;
+  static const birdVelocity = 300;
+  static const gravity = -90.0;
+  static const cloudsHeight = 70.0;
+}
